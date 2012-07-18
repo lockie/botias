e1487d536e5d1db29c865b6b590db95094de3503
Andrew Kravchuk <awkravchuk@gmail.com>
2012-07-18 05:04:43 +0400

    Added xls file parsing and AJAX results

--- application.py
+++ application.py
@@ -3,7 +3,7 @@
 import os
 import hashlib
 from werkzeug import secure_filename
-from flask import Flask, render_template, request, flash, redirect, url_for, session
+from flask import Flask, render_template, request, flash, redirect, url_for, session, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager, login_user, logout_user, login_required, current_user
 from loginform import LoginForm
@@ -17,7 +17,7 @@ SESSION_PROTECTION = 'strong'
 SQLALCHEMY_DATABASE_URI = 'sqlite:////var/www/localhost/htdocs/db/users.db'
 ALLOWED_EXTENSIONS = set(['txt', 'csv', 'xls', 'xlsx'])
 MAX_CONTENT_LENGTH = 64 * 1024 * 1024 # 64M
-UPLOAD_FOLDER = "/tmp" # TODO : deletion?
+UPLOAD_FOLDER = '/tmp'
 
 # construct application
 app = Flask(__name__)
@@ -71,24 +71,30 @@ def allowed_file(filename):
 	return '.' in filename and \
 		filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/result')
+@app.route('/_process')
 @login_required
-def result():
-	return 'kay'
+def process(): # to be called from AJAX
+	file_path = session.pop('current_file', '')
+	if not file_path:
+#		flash('No file submitted')
+#		return redirect(url_for('submit'))
+		return jsonify(result='Error: no file submitted!')
 
-@app.route('/submit', methods=['GET', 'POST'])
-@login_required
-def submit():
-	if request.method == 'POST' and 'data' in request.files:
-		filename = request.files['data'].filename
-		if(allowed_file(filename)):
-			fn = str(current_user.get_id()) + '_' + secure_filename(filename)
-			request.files['data'].save(os.path.join(
-				app.config['UPLOAD_FOLDER'], fn))
-			flash('submit OK')
-			return redirect(url_for('result'))
-	return render_template('submit.html')
+	avg = 0
+	try:
+		import xlrd
+		workbook = xlrd.open_workbook(file_path)
+		for sheet in workbook.sheets():
+			for rown in range(sheet.nrows):
+				row = sheet.row_values(rown)
+				avg += float(row[1]) / sheet.nrows
+	except Exception as e:
+		return jsonify(result='Error: %s' % str(e))
+	finally:
+		os.remove(file_path)
+	return jsonify(result='kay! avg=%f' % avg)
 
+	# TODO : pika
 	context = zmq.Context()
 	socket = context.socket(zmq.REQ)
 	socket.setsockopt(zmq.LINGER, 0)
@@ -105,6 +111,25 @@ def submit():
 	context.term()
 	return render_template('main.html', title='Results', content='Hello, %s!!1' % message)
 
+
+@app.route('/result')
+@login_required
+def result():
+	return render_template('result.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+@login_required
+def submit():
+	if request.method == 'POST' and 'data' in request.files:
+		filename = request.files['data'].filename
+		if(allowed_file(filename)):
+			fn = str(current_user.get_id()) + '_' + secure_filename(filename)
+			path = os.path.join(app.config['UPLOAD_FOLDER'], fn)
+			session['current_file'] = path
+			request.files['data'].save(path)
+			flash('submit OK')
+			return redirect(url_for('result'))
+	return render_template('submit.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
