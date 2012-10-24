035c5a4ab12de8ba69b818b03ea33de497de86bf
Andrew Kravchuk <awkravchuk@gmail.com>
2012-10-24 16:53:40 +0400

    Updated translations

--- registerform.py
+++ registerform.py
@@ -1,38 +1,38 @@
 #!/usr/bin/env python
 
 from flask_wtf import Form, TextField, PasswordField, BooleanField, SelectField, IntegerField, validators
-from flask.ext.babel import gettext, ngettext
+from flask.ext.babel import lazy_gettext as _
 
 
 class RegisterForm(Form):
-	name = TextField(gettext('Name'),
-		[validators.Required(gettext('This field is required.')),
-		validators.Length(min=2, max=25, message=gettext('Wrong length.'))]
+	name = TextField(_('Name'),
+		[validators.Required(_('This field is required.')),
+		validators.Length(min=2, max=25, message=_('Wrong length.'))]
 	)
-	surname = TextField(gettext('Surname'),
-		[validators.Required(gettext('This field is required.')),
-		validators.Length(min=2, max=35, message=gettext('Wrong length.'))]
+	surname = TextField(_('Surname'),
+		[validators.Required(_('This field is required.')),
+		validators.Length(min=2, max=35, message=_('Wrong length.'))]
 	)
-	individual = BooleanField(gettext('Individual'),
-		description=gettext('Are you individual or corporate user'))
+	individual = BooleanField(_('Individual'),
+		description=_('Are you individual or corporate user'))
 	code = TextField('Code',
-		[validators.Required(gettext('This field is required.')),
-		validators.Length(min=8, max=10, message=gettext('Wrong length.'))]
+		[validators.Required(_('This field is required.')),
+		validators.Length(min=8, max=10, message=_('Wrong length.'))]
 	)
-	purpose = SelectField(gettext('Purpose of calculation'), choices=[
-		('edu', gettext('educational purposes')),
-		('aud', gettext('annual audit')),
-		('lst', gettext('introduction of the issuer in exchange listing')),
-		('ipo', gettext('IPO entry'))
+	purpose = SelectField(_('Purpose of calculation'), choices=[
+		('edu', _('educational purposes')),
+		('aud', _('annual audit')),
+		('lst', _('introduction of the issuer in exchange listing')),
+		('ipo', _('IPO entry'))
 	])
-	beneficiary = IntegerField(gettext('Approx. beneficiary count'),
-		[validators.Required(gettext('This field is required.'))]
+	beneficiary = IntegerField(_('Approx. beneficiary count'),
+		[validators.Required(_('This field is required.'))]
 	)
-	email = TextField(gettext('E-mail'),
-		[validators.Required(gettext('This field is required.')),
-		validators.Length(max=80, message=gettext('Wrong length.')),
-		validators.Email(gettext('Invalid email address.'))],
-		description=gettext('We will send you your password to this email.')
+	email = TextField(_('E-mail'),
+		[validators.Required(_('This field is required.')),
+		validators.Length(max=80, message=_('Wrong length.')),
+		validators.Email(_('Invalid email address.'))],
+		description=_('We will send you your password to this email.')
 	)
 
 
