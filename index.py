bc665ee417d64a98f4ddaea359532c7cc1c01dbe
Andrew Kravchuk <awkravchuk@gmail.com>
2012-11-16 18:51:26 +0400

    Added Tornado options parsing

--- index.py
+++ index.py
@@ -7,10 +7,15 @@ from application import app
 #	return ['Hello, world!']
 
 if __name__ == '__main__':
+	from tornado.options import parse_config_file, parse_command_line, options, define
 	from tornado.wsgi import WSGIContainer
 	from tornado.httpserver import HTTPServer
 	from tornado.ioloop import IOLoop
+	define("port", default=80, help="run on the given port", type=int)
+	parse_config_file("app.conf")
+	parse_command_line()
+
 	http_server = HTTPServer(WSGIContainer(app))
-	http_server.listen(80)
+	http_server.listen(options.port)
 	IOLoop.instance().start()
 
