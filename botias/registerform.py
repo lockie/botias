5a5ed2aa5d1cb85b9d5c99dcee7c8abd368c9b95
Andrew Kravchuk <awkravchuk@gmail.com>
2012-12-14 03:13:47 +0400

    Fixes in UI messages (closes #11)

--- botias/registerform.py
+++ botias/registerform.py
@@ -14,25 +14,25 @@ class RegisterForm(Form):
 		validators.Length(min=2, max=35, message=_('Wrong length.'))]
 	)
 	corporate = BooleanField(_('Corporate'),
-		description=_('Are you individual or corporate user'))
+		description=_('Are you corporate user'))
 	code = TextField('Code',
 		[validators.Required(_('This field is required.')),
 		validators.Length(min=8, max=10, message=_('Wrong length.'))]
 	)
-	purpose = SelectField(_('Purpose of calculation'), choices=[
-		('edu', _('educational purposes')),
+	purpose = SelectField(_('Purpose'), choices=[
+		('edu', _('educational')),
 		('aud', _('annual audit')),
 		('lst', _('introduction of the issuer in exchange listing')),
-		('ipo', _('IPO entry'))
+		('ipo', _('IPO'))
 	])
-	beneficiary = IntegerField(_('Approx. beneficiary count'),
+	beneficiary = IntegerField(_('Approximate number of beneficiaries'),
 		[validators.Required(_('This field is required.'))]
 	)
 	email = TextField(_('E-mail'),
 		[validators.Required(_('This field is required.')),
 		validators.Length(max=80, message=_('Wrong length.')),
 		validators.Email(_('Invalid email address.'))],
-		description=_('We will send you your password to this email.')
+		description=_('Your password will be sent to this address.')
 	)
 
 
